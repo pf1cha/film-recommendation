db610a451d1fe21f606657adbaf16f732c63ed67
Max <lonishin.max@gmail.com>
2024-12-08 22:04:23 +0300

    search function

--- database/database.py
+++ database/database.py
@@ -9,6 +9,7 @@ from sqlalchemy.orm import Session
 from database.db_password import DATABASE_URL
 from database.database_info import users_table, content_table
 
+
 Base = declarative_base()
 
 
@@ -33,5 +34,22 @@ def fetch_movies(offset=0, limit=10):
         query = select(content_table).order_by(desc(content_table.c.vote_average)).limit(limit).offset(offset)
         result = session.execute(query).fetchall()
         return result
+    finally:
+        session.close()
+
+
+def fetch_movies_by_title(title: str, limit=None, offset=0):
+    """Fetch movies from the database that contain the specified title."""
+    engine = create_engine(DATABASE_URL)
+    session = Session(bind=engine)
+    try:
+        query = select(content_table).where(content_table.c.name.ilike(f"%{title}%"))
+        if limit is not None:
+            query = query.limit(limit).offset(offset)
+        results = session.execute(query).fetchall()
+        return results
+    except Exception as e:
+        print(f"Error fetching movies: {e}")
+        return []
     finally:
         session.close()
