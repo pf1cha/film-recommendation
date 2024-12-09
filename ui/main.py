117c377271eda815c97a29adbbf78b7f5ab53488
Max <lonishin.max@gmail.com>
2024-12-09 15:33:50 +0300

    add a new main.py

--- ui/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from PyQt6.QtWidgets import QApplication
-from ui.windows.user_interface import MainWindow
-
-if __name__ == "__main__":
-    app = QApplication(sys.argv)
-    main_window = MainWindow()
-    main_window.show()
-    sys.exit(app.exec())
