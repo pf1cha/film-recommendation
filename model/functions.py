2f330b2ca577803077a26bff4563caff82249d4b
Max <lonishin.max@gmail.com>
2024-12-09 17:27:30 +0300

    change the output for funciton

--- model/functions.py
+++ model/functions.py
@@ -13,4 +13,5 @@ def recommend_movies_with_model(movie_id, top_k=5):
     similarities = cosine_similarity(target_embedding.unsqueeze(0), embeddings)
     # Get top_k most similar movies (excluding the target movie)
     similar_movies = similarities.squeeze().argsort(descending=True)[1:top_k + 1]
-    return data.iloc[similar_movies.tolist()]
+    return data.iloc[similar_movies.tolist()]['id']
+
