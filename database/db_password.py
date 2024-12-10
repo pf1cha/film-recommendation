41941a70dd81281e3ba0c5fc9edefb27b1ca553b
Egor Alekseenko <21pepperforreal@gmail.com>
2024-12-10 13:26:54 +0300

    remove password

--- database/db_password.py
+++
@@ -1,4 +0,0 @@
-username = "postgres"
-password = 123456
-# Connection configuration (replace with your actual database credentials)
-DATABASE_URL = f"postgresql+psycopg2://{username}:{password}@localhost:5432/film_db"
